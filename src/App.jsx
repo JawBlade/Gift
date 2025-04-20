36092eb5af54993349a3e6b9ba994caeb66d0fed
vicluca38 <122082998+JawBlade@users.noreply.github.com>
2025-04-20 03:25:32 -0700

    FIrst commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex bg-blue-500 justify-center align-middle">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
