2ae1feefae8b1721d5c982ab38c23cb836921710
vicluca38 <122082998+JawBlade@users.noreply.github.com>
2025-05-05 09:45:11 -0700

    Fixed the Navbar

---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../Components/Navbar.jsx";
+import ImageSlider from "../Components/ImageSlider.jsx";
+
+const Homepage = () => {
+  return (
+    <>
+      <NavBar />
+      <ImageSlider />
+    </>
+  );
+};
+
+export default Homepage;
